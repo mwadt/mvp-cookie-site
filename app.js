df0c261bef88a425c3b7f6db17b511ad5cb5fe14
mwadt <mbrincko93@gmail.com>
2024-04-11 17:07:49 -0700

    added some styling details to the paragraph tag

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const paragraph = document.getElementById('win');
 let buzzer = document.querySelector('#cookieButton');
 console.dir(buzzer);
 
-cookieButton.addEventListener('click', youWin);
-    
-
-
 const youWin = () => {
     alert('You won!');
     paragraph.textContent = `You won a cookie!`;
 }
+
+cookieButton.addEventListener('click', youWin);
+    
+
+
+// const youWin = () => {
+//     alert('You won!');
+//     paragraph.textContent = `You won a cookie!`;
+// }
