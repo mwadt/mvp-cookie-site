a31963b7769a8b66086cbd027234ffc1fe947890
mwadt <mbrincko93@gmail.com>
2024-04-11 17:01:31 -0700

    changed buzzer image to a button

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-let bigButton = 
+const paragraph = document.getElementById('win');
+let buzzer = document.querySelector('#cookieButton');
+console.dir(buzzer);
+
+cookieButton.addEventListener('click', youWin);
+    
+
+
+const youWin = () => {
+    alert('You won!');
+    paragraph.textContent = `You won a cookie!`;
+}
