cfa7d9ff446fa55faf7de4c9df8c0fd5b2941638
mwadt <mbrincko93@gmail.com>
2024-04-15 13:28:56 -0700

    added a timeout to the main buzzer button where if the user doesn't hit the buzzer within 5 seconds, an alert triggers nnotifying them that they lost.

--- app.js
+++ app.js
@@ -10,12 +10,22 @@ console.dir(buzzer);
 //     choiceOutput.appendChild(choiceMessage1);
 // }
 
+const youLose = () => {
+    console.log('You lose');
+    alert('You lose');
+    buzzer.removeEventListener('click', youWin);
+
+}
+
 //lets the user know they won, creates cookie choice buttons
 const youWin = () => {   
-    // console.dir(evt.target.id);
+    
     alert('You won!');
     paragraph.textContent = `You won a cookie!  What kind of cookie would you like?`;
-    buzzer.removeEventListener('click', youWin)
+    clearTimeout(buzzerTimeout);
+
+    
+    
     const cookieChoice1 = document.createElement('button');
     const cookieChoice2 = document.createElement('button');
     const cookieChoice3 = document.createElement('button');
@@ -35,37 +45,41 @@ const youWin = () => {
     newButtons.appendChild(cookieChoice3);
     newButtons.appendChild(cookieChoice4);
 
-    const selectionMessage1 = () => {
-       
-        let choiceMessage1 = document.createElement('p');
-        choiceMessage1.textContent = 'You chose Chocolate Chip!  Excellent Choice!';
-        choiceOutput.appendChild(choiceMessage1);
-       
+
+
+   // function to remove all event listeners from the cookie button choices.  
+    const cookieChoiceEventRemover = () => {
+        cookieChoice1.removeEventListener('click', handleCookieChoiceClick);
+        cookieChoice2.removeEventListener('click', handleCookieChoiceClick);
+        cookieChoice3.removeEventListener('click', handleCookieChoiceClick);
+        cookieChoice4.removeEventListener('click', handleCookieChoiceClick);
     }
-    const selectionMessage2 = () => {
-        let choiceMessage2 = document.createElement('p');
-        choiceMessage2.textContent = `You chose ${}Oatmeal Raisen!  Excellent Choice!`;
-        choiceOutput.appendChild(choiceMessage2);
-    }
-    const selectionMessage3 = () => {
-        let choiceMessage3 = document.createElement('p');
-        choiceMessage3.textContent = 'You chose Sugar Cookie!  Excellent Choice!';
-        choiceOutput.appendChild(choiceMessage3);
-    }
-    con
     
     
+    // function to create a new paragraph depending on the cookie choice.
     const handleCookieChoiceClick = (event) => {
         console.dir(event.target.id);
         if(event.target.id === 'cookieChoice1') {
-            selectionMessage1();
-            cookieChoice1.removeEventListener('click', handleCookieChoiceClick);
+            let choiceMessage = document.createElement('p');
+            choiceMessage.textContent = `You chose a ${cookieChoice1.textContent}!  Excellent Choice!`
+            choiceOutput.appendChild(choiceMessage);
+            cookieChoiceEventRemover();
 
         } else if(event.target.id === 'cookieChoice2') {
-            selectionMessage2();
-            cookieChoice2.removeEventListener('click', handleCookieChoiceClick);
-        } else if(event.target.id === 'cookieChoice3'){
-            selectionMessage3();
+            let choiceMessage = document.createElement('p');
+            choiceMessage.textContent = `You chose a ${cookieChoice2.textContent}!  Excellent Choice!`
+            choiceOutput.appendChild(choiceMessage);
+            cookieChoiceEventRemover();
+        } else if(event.target.id === 'cookieChoice3') {
+            let choiceMessage = document.createElement('p');
+            choiceMessage.textContent = `You chose a ${cookieChoice3.textContent}!  Excellent Choice!`
+            choiceOutput.appendChild(choiceMessage);
+            cookieChoiceEventRemover();
+        } else if(event.target.id === 'cookieChoice4') {
+            let choiceMessage = document.createElement('p');
+            choiceMessage.textContent = `You chose a ${cookieChoice4.textContent}!  Excellent Choice!`
+            choiceOutput.appendChild(choiceMessage);
+            cookieChoiceEventRemover();
         }
         
     }
@@ -74,20 +88,15 @@ const youWin = () => {
     cookieChoice1.addEventListener('click', handleCookieChoiceClick);
     cookieChoice2.addEventListener('click', handleCookieChoiceClick);
     cookieChoice3.addEventListener('click', handleCookieChoiceClick);
+    cookieChoice4.addEventListener('click', handleCookieChoiceClick);
         
 }
 
-
+//adds an event listener for the buzzer
 buzzer.addEventListener('click', youWin);
-    
-// cookieChoice1.addEventListener('click', () => {
-//     console.log('nice choice')
-// })
+//removes the event listener
+buzzerTimeout = setTimeout(youLose, 5000);
 
-// const youWin = () => {
-//     alert('You won!');
-//     paragraph.textContent = `You won a cookie!`;
-// }
 
 
 
